d9fb6501bdf906e2b7bee0ccc8e744b66f29ad2b
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-01-18 13:08:34 +0000

    pass back "infererance_retval" and compute the training_error

--- example_solutions/example_classical_svm_training.py
+++ example_solutions/example_classical_svm_training.py
@@ -1,3 +1,4 @@
+from .helper_functions import infererance_retval
 from sklearn import svm
 import numpy as np
 
@@ -5,15 +6,24 @@ def train_svm(training_example_wfns):
     """This is a train function for any circuit ignoring all quantum properties.
     This will work given enough examples, but well be very slow!
     """
-    clf = svm.SVC()
-    vecs, labels = tuple(zip(*training_example_wfns))
-    vecs = np.array(vecs); vecs = np.concatenate([vecs.real, vecs.imag], axis=1)
+    clf = svm.SVC(gamma='auto')
+    vecs, actual_labels = tuple(zip(*training_example_wfns))
+    vecs = np.array(vecs);
+    vecs = np.concatenate([vecs.real, vecs.imag], axis=1)
 
-    clf.fit(vecs, labels)
+    clf.fit(vecs, actual_labels)
+
+    pred_labels = tuple( clf.predict(vecs) )
+    training_error = 0.0
+    for actual_label, pred_label in zip(actual_labels, pred_labels):
+        training_error += abs(actual_label - pred_label)
 
     # now we create the inference function. This should take a state and produce a prediction.
     def infer(wavefunction):
         wavefunction = np.array(wavefunction).reshape(1, -1)
         return clf.predict(np.concatenate([wavefunction.real, wavefunction.imag], axis=1))[0]
 
-    return infer
+    return infererance_retval(
+        infer_fun = infer,
+        training_error = training_error
+    )
