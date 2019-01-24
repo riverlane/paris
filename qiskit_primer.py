15817f0941abd82314b53499c244450c90a775a0
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-01-24 16:11:49 +0000

    a circuit transforms input states to output states

--- qiskit_primer.py
+++ qiskit_primer.py
@@ -5,15 +5,14 @@ from qiskit import QuantumCircuit, QuantumRegister, BasicAer, execute
 
 ## Setting up:
 #
-num_qubits = 2
+num_qubits = 1
 qr = QuantumRegister(num_qubits, "qr")
 circ = QuantumCircuit(qr)
 
 
 ## Building the circuit:
 #
-circ.x(qr[0])
-circ.h(qr[1])
+circ.x( qr[0] )     # inverting the first and only qubit
 
 
 ## Printing the circuit:
@@ -26,8 +25,13 @@ print( circ.draw().single_string() )
 simulator = BasicAer.get_backend('statevector_simulator')
 
 
-## Running the simulator:
+## Running the simulator on two different input state:
 #
-statevector = execute(circ, simulator).result().get_statevector(circ)
-print( statevector )
+input_state_zero = [1, 0]   # state |0>
+input_state_one  = [0, 1]   # state |1>
+
+for input_statevector in (input_state_zero, input_state_one):
+    print( "Input statevector: {}".format(input_statevector) )
+    output_statevector = execute(circ, simulator, backend_options={"initial_statevector": input_statevector}).result().get_statevector(circ)
+    print( "Output statevector: {}".format(output_statevector) )
 
